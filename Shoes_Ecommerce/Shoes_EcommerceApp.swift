acda83f252fcf4f868c4cfa97f8b5491a70bcdc9
Mahmoud Nagdy <mahmoudnagdy101@gmail.com>
2026-03-21 01:37:46 +0200

    Initial Commit

---
+++ Shoes_Ecommerce/Shoes_EcommerceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shoes_EcommerceApp.swift
+//  Shoes_Ecommerce
+//
+//  Created by Mahmoud Nagdy on 21/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct Shoes_EcommerceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
