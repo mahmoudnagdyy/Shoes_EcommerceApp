40d88711bf8009a9d91c4779b85469cda2fa2e93
Mahmoud Nagdy <mahmoudnagdy101@gmail.com>
2026-03-21 03:12:10 +0200

    Add LoginViewModel

---
+++ Shoes_Ecommerce/Core/Login/ViewModel/LoginViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  LoginViewModel.swift
+//  Shoes_Ecommerce
+//
+//  Created by Mahmoud Nagdy on 21/03/2026.
+//
+
+import Foundation
+internal import Combine
+
+
+@MainActor
+class LoginViewModel: ObservableObject {
+    
+    @Published var email: String = ""
+    @Published var password: String = ""
+    
+}
