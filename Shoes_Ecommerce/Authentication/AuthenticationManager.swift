1102c01ed63f18ad994dcfca6a32fdc74e627a2c
Mahmoud Nagdy <mahmoudnagdy101@gmail.com>
2026-03-21 03:24:15 +0200

    Add AuthenticationManager class to manage Auth Logic

---
+++ Shoes_Ecommerce/Authentication/AuthenticationManager.swift
@@ -0,0 +1,19 @@
+//
+//  AuthenticationManager.swift
+//  Shoes_Ecommerce
+//
+//  Created by Mahmoud Nagdy on 21/03/2026.
+//
+
+import Foundation
+internal import Combine
+
+class AuthenticationManager {
+    
+    static let shared = AuthenticationManager()
+    
+    private init() {}
+    
+    
+    
+}
