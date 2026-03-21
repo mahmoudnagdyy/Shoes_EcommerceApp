6d8ea5a2c029074f4ee58a09010088e532c76a70
Mahmoud Nagdy <mahmoudnagdy101@gmail.com>
2026-03-21 03:01:50 +0200

    Add SignupView

---
+++ Shoes_Ecommerce/Core/Signup/SignupView.swift
@@ -0,0 +1,94 @@
+//
+//  SignupView.swift
+//  Shoes_Ecommerce
+//
+//  Created by Mahmoud Nagdy on 21/03/2026.
+//
+
+import SwiftUI
+
+struct SignupView: View {
+    
+    @StateObject var vm = SignupViewModel()
+    
+    var body: some View {
+        ZStack(alignment: .top) {
+            // background
+            Color(#colorLiteral(red: 0.8862745098, green: 0.4588235294, blue: 0.3254901961, alpha: 1)).ignoresSafeArea()
+            
+            // foreground
+            SignupForeground
+        }
+    }
+}
+
+#Preview {
+    SignupView()
+}
+
+
+extension SignupView {
+    
+    private var SignupForeground: some View {
+        VStack {
+            CreateAnAccountText
+            
+            ZStack(alignment: .top) {
+                // background
+                signupFormBackground
+                
+                // foreground
+                signupFormForeground
+            }
+            
+        }
+    }
+    
+    private var CreateAnAccountText: some View {
+        Text("Create an account")
+            .font(.largeTitle)
+            .foregroundStyle(.white)
+            .fontWeight(.heavy)
+            .frame(maxWidth: 200)
+            .multilineTextAlignment(.center)
+            .padding(30)
+    }
+    
+    private var signupFormBackground: some View {
+        Color.white
+            .cornerRadius(40)
+            .ignoresSafeArea()
+    }
+    
+    private var signupFormForeground: some View {
+        VStack {
+            
+            // next: add google Signin Button
+            
+            nameTextFields
+            
+            EmailTextFieldItem(text: $vm.email)
+            
+            
+            PasswordTextFieldItem(text: $vm.password)
+            
+            SubmitButton(text: "create account", bgColor: .black) {
+                // action
+            }
+            
+            RegisterFooterItem(text: "Have an account?", linkText: "login here") {
+                // action
+            }
+        }
+        .padding(.vertical, 40)
+        .padding(.horizontal)
+    }
+    
+    private var nameTextFields: some View {
+        HStack {
+            NameTextFieldItem(placeholder: "first name", text: $vm.firstName, type: .first)
+            NameTextFieldItem(placeholder: "last name", text: $vm.lastName, type: .last)
+        }
+    }
+    
+}
