3818523bfee0c48574c7c92b39f5298e33036af9
Mahmoud Nagdy <mahmoudnagdy101@gmail.com>
2026-03-21 03:24:44 +0200

    Updating ContentView to switch between screens

--- Shoes_Ecommerce/ContentView.swift
+++ Shoes_Ecommerce/ContentView.swift
@@ -8,17 +8,61 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    enum allScreens {
+        case signup, login
+    }
+    
+    @State var currentScreen: allScreens = .login
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            switch currentScreen {
+            case .login:
+                loginView
+            case .signup:
+                signupView
+            }
+            
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+}
+
+extension ContentView {
+    
+    private var loginView: some View {
+        LoginView(onSignupLinkPressed: {
+            onSignupLinkPressed()
+        })
+        .transition(.move(edge: .leading))
+    }
+    
+    private var signupView: some View {
+        SignupView {
+            onloginLinkPressed()
+        }
+        .transition(.move(edge: .trailing))
+    }
+    
+}
+
+
+extension ContentView {
+    
+    private func onSignupLinkPressed() {
+        withAnimation {
+            currentScreen = .signup
+        }
+    }
+    
+    private func onloginLinkPressed() {
+        withAnimation {
+            currentScreen = .login
+        }
+    }
+    
 }
