3853b348e0bd02a4da4eb222c3804c3c5ce4bed5
Mahmoud Nagdy <mahmoudnagdy101@gmail.com>
2026-03-21 23:15:20 +0200

    Add some features in FirestoreUserManager class

--- Shoes_Ecommerce/Firestore/FirestoreUserManager.swift
+++ Shoes_Ecommerce/Firestore/FirestoreUserManager.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+internal import Combine
+import FirebaseFirestore
+
+class FirestoreUserManager {
+    
+    static let shared = FirestoreUserManager()
+    
+    private let userCollection = Firestore.firestore().collection("users")
+    
+    private init() {}
+    
+    private func userDocument(userID: String) -> DocumentReference {
+        userCollection.document(userID)
+    }
+    
+    func saveUser(user: UserModel) async throws {
+        try await userDocument(userID: user.id).setData(user.userDict)
+    }
+    
+}
