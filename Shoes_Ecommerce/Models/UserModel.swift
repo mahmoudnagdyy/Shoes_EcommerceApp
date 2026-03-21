43fbddc90f37bf0f0f90a96f000f2ac830f5484a
Mahmoud Nagdy <mahmoudnagdy101@gmail.com>
2026-03-21 23:15:38 +0200

    Add UserModel

--- Shoes_Ecommerce/Models/UserModel.swift
+++ Shoes_Ecommerce/Models/UserModel.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+import FirebaseAuth
+
+
+
+struct UserModel: Codable {
+    let id: String
+    let firstName: String
+    let lastName: String
+    let email: String
+    let photoUrl: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case firstName = "first_name"
+        case lastName = "last_name"
+        case email
+        case photoUrl = "photo_url"
+    }
+    
+    
+    var userDict: [String: Any] {
+        return [
+            UserModel.CodingKeys.id.rawValue: id,
+            UserModel.CodingKeys.firstName.rawValue: firstName.lowercased(),
+            UserModel.CodingKeys.lastName.rawValue: lastName.lowercased(),
+            UserModel.CodingKeys.email.rawValue: email.lowercased(),
+            UserModel.CodingKeys.photoUrl.rawValue: photoUrl ?? ""
+        ]
+    }
+}
