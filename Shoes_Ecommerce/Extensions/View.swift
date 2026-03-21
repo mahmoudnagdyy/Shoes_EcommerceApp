a88125b45bd4aa331161b97f4a22fa3705435bb0
Mahmoud Nagdy <mahmoudnagdy101@gmail.com>
2026-03-21 03:02:46 +0200

    Add View Extension

---
+++ Shoes_Ecommerce/Extensions/View.swift
@@ -0,0 +1,26 @@
+//
+//  View.swift
+//  Shoes_Ecommerce
+//
+//  Created by Mahmoud Nagdy on 21/03/2026.
+//
+
+import Foundation
+import SwiftUI
+
+
+extension View {
+    
+    func asTextField() -> some View {
+        modifier(generalTextFieldViewModifier())
+    }
+    
+    func asNameTextField(type: nameType) -> some View {
+        modifier(NameTextFieldViewModifier(type: type))
+    }
+    
+    func asSubmitButton(bgColor: Color) -> some View {
+        modifier(SubmitButtonViewModifier(bgColor: bgColor))
+    }
+    
+}
