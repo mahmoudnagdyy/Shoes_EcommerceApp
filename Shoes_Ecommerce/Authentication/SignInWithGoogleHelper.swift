2a5d4485979f0fb5ed474685c4ea19e694707c9a
Mahmoud Nagdy <mahmoudnagdy101@gmail.com>
2026-03-21 21:41:31 +0200

    add signInWithGoogleHelper class to manage its logic

---
+++ Shoes_Ecommerce/Authentication/SignInWithGoogleHelper.swift
@@ -0,0 +1,19 @@
+//
+//  SignInWithGoogleHelper.swift
+//  Shoes_Ecommerce
+//
+//  Created by Mahmoud Nagdy on 21/03/2026.
+//
+
+import Foundation
+internal import Combine
+
+class SignInWithGoogleHelper {
+    
+    static let shared = SignInWithGoogleHelper()
+    
+    private init() {}
+    
+    
+    
+}
